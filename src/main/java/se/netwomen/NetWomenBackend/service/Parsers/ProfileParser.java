ab219db10bddc4132d342109f930e6fdd871ea73
claudiastenberg <32322760+claudiastenberg@users.noreply.github.com>
2018-06-18 19:27:48 +0200

    HjälpTexterInförParser

--- src/main/java/se/netwomen/NetWomenBackend/service/Parsers/ProfileParser.java
+++ src/main/java/se/netwomen/NetWomenBackend/service/Parsers/ProfileParser.java
@@ -12,4 +12,12 @@ public final class ProfileParser {
     public static Profile toProfile(ProfileDTO profileDTO) {
         return new Profile(profileDTO.getDescription(), UserParser.toUser(profileDTO.getUser()));
     }
+    /* CLAUDIA KOLLA PÅ DETTA
+     public static Post postDTOToPost(PostDTO postDTO) {
+        return new Post(UserParser.toUser(postDTO.getUser()), postDTO.getText(), postDTO.getPictureUrl(), postDTO.getCreationTimestamp());
+    }
+    public static Post profileDTOToProfile(PostDTO postDTO) {
+        return new Post(UserParser.toUser(postDTO.getUser()), postDTO.getText(), postDTO.getPictureUrl(), postDTO.getCreationTimestamp());
+    }
+    */
 }
