4e0ce926fa6f11ff10f32ba17036f8b47a1eb5d1
Anna Wrang <anna.wrang@gmail.com>
2018-06-19 22:41:13 +0100

    small fixes - cookie doesn't work

--- src/main/java/se/netwomen/NetWomenBackend/service/UserService.java
+++ src/main/java/se/netwomen/NetWomenBackend/service/UserService.java
@@ -8,33 +8,37 @@ import se.netwomen.NetWomenBackend.repository.DTO.dto.User.UserDTO;
 import se.netwomen.NetWomenBackend.service.Parsers.UserParser;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserService {
     private final UserRepository repository;
 
     @Autowired
-    public UserService(UserRepository repository){
+    public UserService(UserRepository repository) {
         this.repository = repository;
     }
 
     public User save(User user) {
+        if (user.getUserNumber() == null) {
+            user.setUserNumber(UUID.randomUUID().toString());
+        }
         UserDTO userDTO = UserParser.toUserDTO(user);
-        userDTO =  repository.save(userDTO);
+        userDTO = repository.save(userDTO);
         return user;
     }
 
-    public User findByUserNameAndPassword(String userName, String password) {
-        Optional<UserDTO> userDTO = repository.findByUserNameAndPassword(userName, password);
-        if(userDTO.isPresent()){
+    public User findByEmailAndPassword(String email, String password) {
+        Optional<UserDTO> userDTO = repository.findByEmailAndPassword(email, password);
+        if (userDTO.isPresent()) {
             return UserParser.toUser(userDTO.get());
         }
         return null;
     }
 
     public void setCookie(String userName, String password, String cookie) {
-        Optional<UserDTO> optionalUser = repository.findByUserNameAndPassword(userName, password);
-        if (optionalUser.isPresent()){
+        Optional<UserDTO> optionalUser = repository.findByEmailAndPassword(userName, password);
+        if (optionalUser.isPresent()) {
             optionalUser.get().setCookie(cookie);
             repository.save(optionalUser.get());
         }
